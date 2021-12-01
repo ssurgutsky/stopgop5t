bb4b7757553ebe792ea0871502a3d15595db29df
ssurgutsky <ssurgutsky@gmail.com>
2021-12-02 01:40:29 +0700

    - init

---
+++ src/components/Settings.js
@@ -0,0 +1,28 @@
+/* eslint no-eval: 0 */
+
+export default {
+  // Increase this on every update to reload assets for new on startup
+  GAME_VERSION: 34,
+  // Release = true
+  CACHE_ENABLED: true,
+
+  GAME_VERSION_VAR_NAME: 'game_version',
+  GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
+  GAME_SAVE_POINT: 'GAME_SAVEPOINT',
+  // Goto this mark if not found player's one on LOAD button pressing
+  DEFAULT_LOAD_MARK_NAME: 'START',
+  // Just a name of Index DB for this game
+  INDEXEDDB_STORE_NAME: 'store_sg5',
+  // Google Play IAP
+  GP_PRODUCT1_NAME: 'com.skystairgames.sg5.coin1',
+  GP_PRODUCT2_NAME: 'com.skystairgames.sg5.episodes',
+  // You can enable some buttons by default in the regular game
+  ENABLE_SAVE_BUTTON: false,
+  ENABLE_LOAD_BUTTON: false,
+  ENABLE_SKIP_BUTTON: false,
+  ENABLE_EPISODES_BUTTON: false,
+  ENABLE_BACK_BUTTON: false,
+  ENABLE_DONATE_BUTTON: true,
+
+  ENABLE_SPEECH: false // window.speechSynthesis !== undefined
+}
